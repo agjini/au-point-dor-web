1a6c923c1257c8a7794ea20fd06a4b35f6bacb0a
Augustin Gjini <augustin.gjini@gmail.com>
2016-04-17 10:46:27 +0200

    Add google analytics js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
     "use strict";
 
     $('body').scrollspy({
@@ -11,26 +11,39 @@
             top: 200
         }
     });
-    
+
     new WOW().init();
-    
-    $('a.page-scroll').bind('click', function(event) {
+
+    $('a.page-scroll').bind('click', function (event) {
         var $ele = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($ele.attr('href')).offset().top - 60)
         }, 1450, 'easeInOutExpo');
         event.preventDefault();
     });
-    
-    $('.navbar-collapse ul li a').click(function() {
+
+    $('.navbar-collapse ul li a').click(function () {
         /* always close responsive nav after click */
         $('.navbar-toggle:visible').click();
     });
 
     $('#galleryModal').on('show.bs.modal', function (e) {
-       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+        $('#galleryImage').attr("src", $(e.relatedTarget).data("src"));
     });
 
 })(jQuery);
 
-
+// Google analytics
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r;
+    i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+    a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0];
+    a.async = 1;
+    a.src = g;
+    m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', 'UA-76530120-1', 'auto');
+ga('send', 'pageview');
